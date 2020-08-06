5d54a571adb8e5f78bbd2a027d698c1600473b15
Glauber Charles <glauber.charles2012@gmail.com>
2020-08-06 11:22:23 -0300

    Seeding Service

--- SalesWebMVC/Models/Enums/SaleStatus.cs
+++ SalesWebMVC/Models/Enums/SaleStatus.cs
@@ -7,8 +7,8 @@ namespace SalesWebMVC.Models.Enums
 {
     public enum SaleStatus : int
     {
-        PENDING = 0,
-        BILLED = 1,
-        CANCELED = 2
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
     }
 }
